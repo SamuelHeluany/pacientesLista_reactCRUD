eec14b63e3157e70b6e2dace366a68e6a8fcd845
Samuel Heluany <samuelheluany@hotmail.com>
2021-08-12 11:14:13 -0300

    Secound commit

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import FormCadastro from './FormCadastro';
+import fireDb from '../database/firebase'
 
 const Cadastro = () => {
+
+    const addEdit = obj => {
+        fireDb.child('pacientes').push(
+            obj,
+            error => {
+                if(error) {
+                    console.log(error)
+                }
+            }
+        )
+    }
+
     return (
         <div>
         
-        <div className="h-100 p-5 text-white bg-dark rounded-3">
+        <div className="h-100 p-5 text-white bg-primary rounded-3">
             <div className="container">
                 <h2>Cadastro de Pacientes</h2>
                 <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
@@ -14,12 +27,11 @@ const Cadastro = () => {
         
         <div className="row">
            <div className="col">
-               <FormCadastro />
+               <FormCadastro addEdit={addEdit} />
            </div>
            <div className="col">
                 <h2>Lista de pacientes</h2>
            </div>
-           
         </div>
         </div>
     )
--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import Cadastro from './Cadastro'
 
-const FormCadastro = () => {
+const FormCadastro = (props) => {
     // captura de dados
     const camposIniciaisDeValores = {
         nomeCompleto: '',
@@ -9,9 +10,74 @@ const FormCadastro = () => {
         endereco: ''
     }
 
-    let { value, setValue} = useState(camposIniciaisDeValores)
+    let [values, setValues] = useState(camposIniciaisDeValores)
+
+    const manipuladorInputChange = e => {
+        let { name, value } = e.target
+
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+
+    const manipuladorFormEnvio = e => {
+        e.preventDefault()
+        props.addEdit(values)
+    }
+
     return (
-        <h1>FormCadastro</h1>
+        <form autoComplete="off" onSubmit={manipuladorFormEnvio}>
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">
+                    <i className="fas fa-user"></i>
+                </span>
+                <input type="text" className="form-control"
+                    placeholder="Nome Completo" name="nomeCompleto"
+                    value={values.nomeCompleto}
+                    onChange={manipuladorInputChange} />
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">
+                            <i className="fas fa-mobile-alt"></i>
+                        </span>
+                        <input type="text" className="form-control"
+                            placeholder="Telefone" name="telefone"
+                            value={values.telefone}
+                            onChange={manipuladorInputChange} />
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">
+                            <i className="fas fa-envelope"></i>
+                        </span>
+                        <input type="text" className="form-control"
+                            placeholder="E-mail" name="email"
+                            value={values.email}
+                            onChange={manipuladorInputChange} />
+                    </div>
+                </div>
+
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">
+                        <i className="far fa-address-card"></i>
+                    </span>
+                    <input type="text" className="form-control"
+                        placeholder="Endereço" name="endereco"
+                        value={values.endereco}
+                        onChange={manipuladorInputChange} />
+                </div>
+
+                <div className="d-grid gap-2 ">
+                    <input type="submit" value="Cadastrar" className="btn btn-primary" />
+                </div>
+            </div>
+        </form>
     )
 }
 
